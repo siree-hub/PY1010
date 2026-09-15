ed8b1aa4a714b85ae8a323173b4dad5d421511ea
siree-hub <sireesaether@gmail.com>
2026-09-15 20:04:54 +0200

    Add files via upload

---
+++ from C to F v.1.py
@@ -0,0 +1,20 @@
+"""
+Slik kan man skrive tekst, med tegnene over
+
+Omregning fra C til F
+
+Siree Sæther. 2026 09 01
+"""
+#PEP 8 skriveregler for python
+
+# #%% betyr kodecelle, med overskrift. Du kan få en innholdsfortegnelse ved å trykke Window - Panes - Outline
+
+#%% Data
+C = 100  # [celcius] 
+
+#%% Beregning
+F = C*1.8+32  # [Fahrenheit] 
+
+
+#%% Utskrift
+print("F =", F)  #Python lager et eget mellomrom når den viser verdi for enkeltargumenter
